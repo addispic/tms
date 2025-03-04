82ad5e048c9b0360f5163cdea64a2d3417ac0181
addispic <addispik@gmail.com>
2025-03-04 08:23:35 +0300

    changes

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,4 @@
 // base url
-export const BASE_URL = "http://localhost:5000";
+// export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = "https://ticketmanagement-server.vercel.app";
+
