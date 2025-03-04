b0ded6f32f4f8fa6eae02406e0528fb3c6c4eca8
addispic <addispik@gmail.com>
2025-03-04 13:18:29 +0300

    changes

--- frontend/src/utils/togglers.ts
+++ frontend/src/utils/togglers.ts
@@ -1,8 +1,8 @@
-const leftSideBar = document.getElementById("left-sidebar");
-const rightSideBar = document.getElementById("right-sidebar");
 // left side bar
 export const leftSideBarToggler = () => {
   console.log("From The Function Itself");
+  const leftSideBar = document.getElementById("left-sidebar");
+  console.log(leftSideBar);
   if (leftSideBar?.classList.contains("absolute")) {
     if (leftSideBar.classList.contains("w-0")) {
       leftSideBar.classList.remove("w-0");
@@ -15,6 +15,7 @@ export const leftSideBarToggler = () => {
 };
 // right side bar toggler
 export const rightSideBarToggler = () => {
+  const rightSideBar = document.getElementById("right-sidebar");
   if (rightSideBar?.classList.contains("absolute")) {
     if (rightSideBar.classList.contains("w-0")) {
       rightSideBar.classList.remove("w-0");
